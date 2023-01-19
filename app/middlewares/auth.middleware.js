029f221f7912038e302551aef2fa3e37a7b1d449
ivanignatius <ivanignatius98@gmail.com>
2023-01-19 23:22:28 +0700

    change message

--- app/middlewares/auth.middleware.js
+++ app/middlewares/auth.middleware.js
@@ -10,7 +10,7 @@ module.exports = {
   verifyToken: (req, res, next) => {
     let token = req.headers["x-access-token"]
     if (!token) {
-      return res.locals.helpers.jsonFormat(403, 'No token provided!')
+      return res.locals.helpers.jsonFormat(403, 'No token header provided!')
     }
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
       if (err) {
